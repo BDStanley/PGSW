4a20c609878bc11bf99a7bd81a6154149e6ae8c2
BDStanley <bdstanley@gmail.com>
2020-01-15 13:30:59 +0100

    15 January

--- PGSW 2019.R
+++ PGSW 2019.R
@@ -2,6 +2,7 @@
 rm(list=ls())
 library(plyr); library(tidyverse); library(sjlabelled); library(labelled); library(scales); 
 library(statar); library(lavaan); library(poLCA); library(sjPlot); library(googledrive)
+1
 
 #Download and read data
 import <- drive_download(as_id('https://drive.google.com/open?id=1jJwiIn-6z2DmdN-N2DX72wYi_nScbqzx'), overwrite=TRUE)
