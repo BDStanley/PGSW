60f084761f437f7584031077bc2a7b78fbf81e88
BDStanley <bdstanley@gmail.com>
2020-02-26 23:37:24 +0100

    26 Feb

--- PGSW 2019 analyses.R
+++ PGSW 2019 analyses.R
@@ -1,8 +1,9 @@
 load("PGSW2019.RData")
-library(tidyverse); library(psych); library(statar); library(scales); library(nnet); library(sjlabelled); 
-library(labelled); library(ggeffects); library(FactoMineR)
+library(tidyverse); library(easystats); library(ggeffects); library(rio); library(scales); library(sjlabelled); library(labelled); library(sjPlot); library(report)
+library(FactoMineR); library(psych); library(nnet)
 
 #Polychoric factor analysis of populism and simplism variables
+#data preparation
 S1_1 <- as.numeric(unlist(pgsw2019$simp_1))
 S1_2 <- as.numeric(unlist(pgsw2019$simp_2))
 S1_3 <- as.numeric(unlist(pgsw2019$simp_3))
@@ -15,63 +16,24 @@ P19_3 <- as.numeric(unlist(pgsw2019$crisis))
 P19_4 <- as.numeric(unlist(pgsw2019$morals))
 P19_5 <- as.numeric(unlist(pgsw2019$simple))
 P19_6 <- as.numeric(unlist(pgsw2019$goodev))
-
 weight <- as.numeric(unlist(pgsw2019$weight))
-
 popsimp <- data.frame(S1_1, S1_2, S1_3, S1_4, S1_5, S1_6,
                       P19_1, P19_2, P19_3, P19_4, P19_5, P19_6)
 
-popsimp_poly <- polychoric(popsimp)
-parallel <- fa.parallel(popsimp_poly$rho, cor="poly", fm="minres", fa="fa", main = "Scree Plot")
-obs = data.frame(parallel$fa.values)
-obs$type = c('Observed Data')
-obs$num = c(row.names(obs))
-obs$num = as.numeric(obs$num)
-colnames(obs) = c('eigenvalue', 'type', 'num')
-percentile = apply(parallel$values,2,function(x) quantile(x,.95))
-min = as.numeric(nrow(obs))
-min = (4*min) - (min-1)
-max = as.numeric(nrow(obs))
-max = 4*max
-percentile1 = percentile[min:max]
-sim = data.frame(percentile1)
-sim$type = c('Simulated Data (95th %ile)')
-sim$num = c(row.names(obs))
-sim$num = as.numeric(sim$num)
-colnames(sim) = c('eigenvalue', 'type', 'num')
-eigendat = rbind(obs,sim)
+#factor analysis
+poly_model <- fa(popsimp, nfactor=2, cor="poly", fm="ml", rotate = "varimax", correct=0, n.obs=2003, weight=weight)
 
-apatheme=theme_bw()+
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_blank(),
-        text=element_text(family='Arial'),
-        legend.title=element_blank(),
-        legend.position=c(.7,.8),
-        axis.line.x = element_line(color='black'),
-        axis.line.y = element_line(color='black'))
+#interpretation and presentation of results
+poly_model_res <- parameters(poly_model, threshold=0.3, sort=TRUE)
+factor.groups <- sjt.pca(popsimp, nmbr.fctr = 2)$factor.index
+item_analysis <- tab_itemscale(popsimp, factor.groups)
+labs <- get_labels(pgsw2019$simp_1)
+likert_plot <- plot_likert(popsimp, groups=factor.groups, catcount=5, 
+                           sort.frq="pos.asc", reverse.scale = TRUE, 
+                           groups.titles = c("Simplism", "Populism"),
+                           legend.labels = labs)
 
-p = ggplot(eigendat, aes(x=num, y=eigenvalue, shape=type)) +
-  #Add lines connecting data points
-  geom_line()+
-  #Add the data points.
-  geom_point(size=4)+
-  #Label the y-axis 'Eigenvalue'
-  scale_y_continuous(name='Eigenvalue')+
-  #Label the x-axis 'Factor Number', and ensure that it ranges from 1-max # of factors, increasing by one with each 'tick' mark.
-  scale_x_continuous(name='Factor Number', breaks=min(eigendat$num):max(eigendat$num))+
-  #Manually specify the different shapes to use for actual and simulated data, in this case, white and black circles.
-  scale_shape_manual(values=c(16,1)) +
-  #Add vertical line indicating parallel analysis suggested max # of factors to retain
-  geom_vline(xintercept = parallel$nfact, linetype = 'dashed')+
-  #Apply our apa-formatting theme
-  apatheme
-
-
-plot_pca <- PCA(popsimp)
-poly_model <- fa(popsimp, nfactor=2, cor="poly", fm="minres", rotate = "varimax", correct=0, n.obs=2003, weight=weight)
-
+#factor scores
 scores <- predict(poly_model, data=popsimp)
 pgsw2019$simplism <- scales::rescale(scores[,1], c(0,1))
 pgsw2019$simplism <- set_labels(pgsw2019$simplism, labels = c("Low" = 0, "High" = 1))
@@ -80,8 +42,9 @@ pgsw2019$populism <- scales::rescale(scores[,2], c(0,1))
 pgsw2019$populism <- set_labels(pgsw2019$populism, labels = c("Low" = 0, "High" = 1))
 var_label(pgsw2019$populism) <- "Level of populism"
 
-model_reg <- lm(simplism ~ populism, data=pgsw2019, weight=weight)
-marg_reg <- ggemmeans(model_reg, terms=c("populism"))
+#example models
+model_reg <- lm(populism ~ simplism, data=pgsw2019, weight=weight)
+marg_reg <- ggemmeans(model_reg, terms=c("simplism"))
 
-model <- multinom(votefor ~ simplism + populism, data=pgsw2019, weight=weight)
-marg <- ggemmeans(model, terms=c("populism", "simplism"))
+model_mnl <- multinom(votefor ~ simplism + populism, data=pgsw2019, weight=weight)
+marg_mnl <- ggemmeans(model, terms=c("populism", "simplism"))
