9594fabf58eb115e02fbf0f1c43d72850fde3f92
Ben Stanley <benstanley@Bens-MacBook-Air.local>
2020-02-28 10:36:55 +0100

    28 Feb 1

--- PGSW 2019 analyses.R
+++ PGSW 2019 analyses.R
@@ -1,77 +1,59 @@
 load("PGSW2019.RData")
-library(tidyverse); library(psych); library(statar); library(scales); library(nnet); library(sjlabelled); 
-library(labelled); library(ggeffects); library(FactoMineR)
+library(tidyverse); library(easystats); library(ggeffects); library(rio); library(scales); library(sjlabelled); library(labelled); library(sjPlot); library(report)
+library(FactoMineR); library(psych); library(nnet)
 
 #Polychoric factor analysis of populism and simplism variables
-S1_1 <- as.numeric(unlist(pgsw2019$simp_1))
-S1_2 <- as.numeric(unlist(pgsw2019$simp_2))
-S1_3 <- as.numeric(unlist(pgsw2019$simp_3))
-S1_4 <- as.numeric(unlist(pgsw2019$simp_4))
-S1_5 <- as.numeric(unlist(pgsw2019$simp_5))
-S1_6 <- as.numeric(unlist(pgsw2019$simp_6))
-P19_1 <- as.numeric(unlist(pgsw2019$neword))
-P19_2 <- as.numeric(unlist(pgsw2019$ordpeop))
-P19_3 <- as.numeric(unlist(pgsw2019$crisis))
-P19_4 <- as.numeric(unlist(pgsw2019$morals))
-P19_5 <- as.numeric(unlist(pgsw2019$simple))
-P19_6 <- as.numeric(unlist(pgsw2019$goodev))
+#data preparation
+S1_1 <- as.numeric(unlist(pgsw2019$simp_1)) %>%
+  set_label("Politycy zamiast skutecznie działać ciągle gmatwają proste sprawy.")
+S1_2 <- as.numeric(unlist(pgsw2019$simp_2)) %>%
+  set_label("Naukowcy i eksperci różnych dziedzin zbytnio komplikują sprawy, które są dość proste.")
+S1_3 <- as.numeric(unlist(pgsw2019$simp_3)) %>%
+  set_label("Obecnie medycyna zamiast pomagać tylko mąci ludziom w głowach; by być zdrowym wystarczy żyć naturalnie i słuchać ludzi podobnych do nas.")
+S1_4 <- as.numeric(unlist(pgsw2019$simp_4)) %>%
+  set_label("Rozwiązanie problemów naszego kraju jest bardzo prostą rzeczą, trzeba tylko dać władze tym, którzy będą chcieli tego dokonać.")
+S1_5 <- as.numeric(unlist(pgsw2019$simp_5)) %>%
+  set_label("Prace parlamentu to niekończące się debatowanie nad szczegółami ustaw, a tymczasem regulacje niektórych spraw są tak proste, że każdy z nas mógłby je rozwiązać.")
+S1_6 <- as.numeric(unlist(pgsw2019$simp_6)) %>%
+  set_label("W polityce toczy się ostra walka między dobrem, reprezentowanym przez zwykłych ludzi, a złem – wynikającym z działań często skorumpowanych elit.")
+
+P12_1 <- as.numeric(unlist(pgsw2019$neword)) %>%
+  set_label("Polsce potrzebny jest ktoś, kto będzie miał dość siły, by zmienić całkowicie nasz system władzy i zaprowadzić nowy, sprawiedliwy ład i porządek.")
+P12_2 <- as.numeric(unlist(pgsw2019$ordpeop)) %>%
+  set_label("W Polsce nieliczni zawłaszczają władzę, przynależną zwykłym ludziom.")
+P12_3 <- as.numeric(unlist(pgsw2019$crisis)) %>%
+  set_label("Jest ostatnia chwila, by uchronić Polskę przed grożącą katastrofą.")
+P12_4 <- as.numeric(unlist(pgsw2019$morals)) %>%
+  set_label("W Polsce nic się nie poprawi, dopóki politycy nie powrócą do starych, dobrych wartości moralnych.")
+P12_5 <- as.numeric(unlist(pgsw2019$simple)) %>%
+  set_label("Rozwiązanie problemów naszego kraju jest bardzo prostą rzeczą, trzeba tylko dać władzę tym, którzy będą chcieli tego dokonać.")
+P12_6 <- as.numeric(unlist(pgsw2019$goodev)) %>%
+  set_label("Wszystko w polityce jest jednoznacznie dobre lub złe, wybór jest jasny.")
 
 weight <- as.numeric(unlist(pgsw2019$weight))
-
 popsimp <- data.frame(S1_1, S1_2, S1_3, S1_4, S1_5, S1_6,
-                      P19_1, P19_2, P19_3, P19_4, P19_5, P19_6)
+                      P12_1, P12_2, P12_3, P12_4, P12_5, P12_6)
+varlab <- get_label(popsimp)
 
-popsimp_poly <- polychoric(popsimp)
-parallel <- fa.parallel(popsimp_poly$rho, cor="poly", fm="minres", fa="fa", main = "Scree Plot")
-obs = data.frame(parallel$fa.values)
-obs$type = c('Observed Data')
-obs$num = c(row.names(obs))
-obs$num = as.numeric(obs$num)
-colnames(obs) = c('eigenvalue', 'type', 'num')
-percentile = apply(parallel$values,2,function(x) quantile(x,.95))
-min = as.numeric(nrow(obs))
-min = (4*min) - (min-1)
-max = as.numeric(nrow(obs))
-max = 4*max
-percentile1 = percentile[min:max]
-sim = data.frame(percentile1)
-sim$type = c('Simulated Data (95th %ile)')
-sim$num = c(row.names(obs))
-sim$num = as.numeric(sim$num)
-colnames(sim) = c('eigenvalue', 'type', 'num')
-eigendat = rbind(obs,sim)
+mvn(popsimp)
+KMO(popsimp)
+cortest.bartlett(popsimp)
+scree <- scree(popsimp)
+parallel <- fa.parallel(popsimp, fm = "pa", fa="both", cor="poly", correct=0)
 
-apatheme=theme_bw()+
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        panel.border = element_blank(),
-        text=element_text(family='Arial'),
-        legend.title=element_blank(),
-        legend.position=c(.7,.8),
-        axis.line.x = element_line(color='black'),
-        axis.line.y = element_line(color='black'))
+#factor analysis
+poly_model <- fa(popsimp, nfactor=3, cor="poly", fm="ml", rotate = "varimax", correct=0, n.obs=2003, weight=weight)
 
-p = ggplot(eigendat, aes(x=num, y=eigenvalue, shape=type)) +
-  #Add lines connecting data points
-  geom_line()+
-  #Add the data points.
-  geom_point(size=4)+
-  #Label the y-axis 'Eigenvalue'
-  scale_y_continuous(name='Eigenvalue')+
-  #Label the x-axis 'Factor Number', and ensure that it ranges from 1-max # of factors, increasing by one with each 'tick' mark.
-  scale_x_continuous(name='Factor Number', breaks=min(eigendat$num):max(eigendat$num))+
-  #Manually specify the different shapes to use for actual and simulated data, in this case, white and black circles.
-  scale_shape_manual(values=c(16,1)) +
-  #Add vertical line indicating parallel analysis suggested max # of factors to retain
-  geom_vline(xintercept = parallel$nfact, linetype = 'dashed')+
-  #Apply our apa-formatting theme
-  apatheme
+#interpretation and presentation of results
+poly_model_res <- parameters(poly_model, threshold=0.3, labels=varlab)
+factor.groups <- sjt.fa(popsimp, nmbr.fctr = 3)$factor.index
+item_analysis <- tab_itemscale(popsimp, factor.groups)
+labs <- get_labels(pgsw2019$simp_1)
+likert_plot <- plot_likert(popsimp, groups=factor.groups, catcount=5, 
+                           sort.frq="pos.asc", reverse.scale = TRUE,
+                           legend.labels = labs)
 
-
-plot_pca <- PCA(popsimp)
-poly_model <- fa(popsimp, nfactor=2, cor="poly", fm="minres", rotate = "varimax", correct=0, n.obs=2003, weight=weight)
-
+#factor scores
 scores <- predict(poly_model, data=popsimp)
 pgsw2019$simplism <- scales::rescale(scores[,1], c(0,1))
 pgsw2019$simplism <- set_labels(pgsw2019$simplism, labels = c("Low" = 0, "High" = 1))
@@ -80,8 +62,9 @@ pgsw2019$populism <- scales::rescale(scores[,2], c(0,1))
 pgsw2019$populism <- set_labels(pgsw2019$populism, labels = c("Low" = 0, "High" = 1))
 var_label(pgsw2019$populism) <- "Level of populism"
 
-model_reg <- lm(simplism ~ populism, data=pgsw2019, weight=weight)
-marg_reg <- ggemmeans(model_reg, terms=c("populism"))
+#example models
+model_reg <- lm(populism ~ simplism, data=pgsw2019, weight=weight)
+marg_reg <- ggemmeans(model_reg, terms=c("simplism"))
 
-model <- multinom(votefor ~ simplism + populism, data=pgsw2019, weight=weight)
-marg <- ggemmeans(model, terms=c("populism", "simplism"))
+model_mnl <- multinom(votefor ~ simplism + populism, data=pgsw2019, weight=weight)
+marg_mnl <- ggemmeans(model_mnl, terms=c("populism", "simplism"))
